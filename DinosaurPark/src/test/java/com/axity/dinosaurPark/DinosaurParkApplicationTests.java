f190a52b28040811aa6b1ba2fbcdaa364bd2425c
JMaRod <kenkut1@gmail.com>
2026-05-20 19:32:45 -0600

    baseTerminada

---
+++ DinosaurPark/src/test/java/com/axity/dinosaurPark/DinosaurParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axity.dinosaurPark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinosaurParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
